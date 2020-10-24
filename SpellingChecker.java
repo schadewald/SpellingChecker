496ff69f8e647b4ee70a8679d84c20c60507fd2f
Chad Schadewald <ces83@students.uwf.edu>
2020-10-23 20:58:49 -0500

    Working it out.

---
+++ SpellingChecker.java
@@ -0,0 +1,10 @@
+public class SpellingChecker 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("");
+        System.out.println("Testing .bat file implementation.");
+        System.out.println("");
+        System.out.println("");
+    }
+}
---
+++ run.bat
@@ -0,0 +1,2 @@
+javac SpellingChecker.java
+java SpellingChecker
