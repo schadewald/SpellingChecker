e1750ef3238380694d01fb38f64d40f3b6097a97
Chad Schadewald <ces83@students.uwf.edu>
2020-10-24 21:57:34 -0500

    TextArea up.

--- src/Controller.java
+++ src/Controller.java
@@ -1,13 +1,11 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.control.MenuBar;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
-
 import java.io.File;
+import java.io.FileWriter;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,20 +13,28 @@ public class Controller implements Initializable
 {
     @FXML
     private MenuBar menuBar;
-    @FXML
-    public void handleMouseClick(MouseEvent event)
-    {
-        System.out.println("Testing");
-    }
-    @FXML
+    public TextArea textArea;
     public void openFile(ActionEvent event)
     {
-        System.out.println("Testing Testing Testing");
+        System.out.println("Open File Clicked");
         FileChooser fileChooser = new FileChooser();
         fileChooser.setInitialDirectory(new File("."));
         File file = fileChooser.showOpenDialog(null);
         if(file != null)
             System.out.println(file.getPath());
+    }
+    public void saveFile(ActionEvent event)
+    {
+        System.out.println("Save File Clicked");
+//        FileWriter fw = new FileWriter()
+    }
+    public void exitFile(ActionEvent event)
+    {
+        System.out.println("Exit File Clicked");
+    }
+    public void spellCheck(ActionEvent event)
+    {
+        System.out.println("Spell Check Clicked");
     }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,8 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
-import java.io.File;
 
 public class Main extends Application
 {
@@ -19,62 +10,9 @@ public class Main extends Application
     public void start(Stage primaryStage) throws Exception //TODO:: add catch
     {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
-        MenuBar menuBar = new MenuBar();
-        Menu fileMenu = new Menu("File");
-        Menu editMenu = new Menu("Edit");
-        MenuItem openFileItem = createOpenFileItem();
-        MenuItem saveFileItem = createSaveFileItem();
-        MenuItem exitItem = createExitItem();
-        fileMenu.getItems().addAll(openFileItem, saveFileItem, exitItem);
-        menuBar.getMenus().addAll(fileMenu, editMenu);
-
         primaryStage.setTitle("Welcome to Spelling Checker!");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-    }
-    private MenuItem createOpenFileItem()
-    {
-        MenuItem openFileItem = new MenuItem("Open");
-        openFileItem.setOnAction(new EventHandler<ActionEvent>()
-        {
-            @Override
-            public void handle(ActionEvent event)
-            {
-                FileChooser fileChooser = new FileChooser();
-                fileChooser.setInitialDirectory(new File("."));
-                File file = fileChooser.showOpenDialog(null);
-                if(file != null)
-                    System.out.println(file.getPath());
-            }
-        });
-        return openFileItem;
-    }
-    private MenuItem createSaveFileItem()
-    {
-        MenuItem createSaveItem = new MenuItem("Save");
-        createSaveItem.setOnAction(new EventHandler<ActionEvent>()
-        {
-            @Override
-            public void handle(ActionEvent event)
-            {
-                System.out.println("Save Clicked!");
-            }
-        });
-        return createSaveItem;
-    }
-    private MenuItem createExitItem()
-    {
-        MenuItem createExitItem = new MenuItem("Exit");
-        createExitItem.setOnAction(new EventHandler<ActionEvent>()
-        {
-            @Override
-            public void handle(ActionEvent event)
-            {
-                System.out.println("Exit Clicked!");
-            }
-        });
-        return createExitItem;
     }
     public static void main(String[] args)
     {
