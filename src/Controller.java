ade5147454cb15388b7424a487b71ce30e75a750
Chad Schadewald <ces83@students.uwf.edu>
2020-10-24 20:28:11 -0500

    Working on opening a file.

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,38 @@
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.control.MenuBar;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable
+{
+    @FXML
+    private MenuBar menuBar;
+    @FXML
+    public void handleMouseClick(MouseEvent event)
+    {
+        System.out.println("Testing");
+    }
+    @FXML
+    public void openFile(ActionEvent event)
+    {
+        System.out.println("Testing Testing Testing");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialDirectory(new File("."));
+        File file = fileChooser.showOpenDialog(null);
+        if(file != null)
+            System.out.println(file.getPath());
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        menuBar.setFocusTraversable(true);
+    }
 }
